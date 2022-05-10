555bc5453158b55c8feac75d09097308c2c91103
Lanie Williams <lanie_williams@baylor.edu>
2022-05-10 14:40:02 -0500

    adding more notes

--- users/views.py
+++ users/views.py
@@ -20,7 +20,7 @@ def register(request):
             new_user = form.save()
         # Log the user in and then redirect to home page.
             login(request, new_user)
-            return redirect('FeedApp:profile')
+            return redirect('FeedApp:index')
 
 # Display a blank or invalid form.
     context = {'form': form}
